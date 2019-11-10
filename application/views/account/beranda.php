619cc4f769705f6305ee6c556cd93cde981b1e18
jayker1 <muhamad.gani33@gmail.com>
2019-11-11 05:32:19 +0700

    K-System

---
+++ application/views/account/beranda.php
@@ -0,0 +1,21 @@
+ <?php
+ defined('BASEPATH') OR exit('No direct script access allowed');
+ ?><!DOCTYPE html>
+ <html>
+ <head>
+     <meta charset="UTF-8">
+     <title>
+        APLIKASI MANAJEMEN UNIT KEGIATAN MAHASISWA K-RADIO (K-SYSTEM)
+     </title>
+ </head>
+ <body>
+     <h1>Welcome to K-SYSTEM</h1>
+     <p>  
+     Klik disini untuk 
+     <?php echo anchor('login','Login'); ?>
+     dan klik
+     <?php echo anchor('register','Daftar'); ?>
+     untuk mendaftar.
+     </p>      
+ </body>
+ </html>
